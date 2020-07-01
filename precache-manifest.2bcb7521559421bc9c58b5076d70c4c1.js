18a848b12c6d7a3d0fab734767fe5df6bec95974
Nicole Eric Cabunilas <necabunilas@gmail.com>
2020-07-01 19:48:14 +0800

    Updates

---
+++ precache-manifest.2bcb7521559421bc9c58b5076d70c4c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db45a7eb6bce25f8a57483ef22d5d283",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "5241e278459a185cf61e",
+    "url": "/todo-react/static/css/main.5f026f31.chunk.css"
+  },
+  {
+    "revision": "879789c3776f5d215a38",
+    "url": "/todo-react/static/js/2.c1cba826.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/todo-react/static/js/2.c1cba826.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5241e278459a185cf61e",
+    "url": "/todo-react/static/js/main.0feedaab.chunk.js"
+  },
+  {
+    "revision": "2d334d22ad330ed9abb3",
+    "url": "/todo-react/static/js/runtime-main.cbaed561.js"
+  }
+]);
